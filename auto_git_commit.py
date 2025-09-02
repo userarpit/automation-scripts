d22e14cca9ff493d8e11c3bf9fa3c1e5fbd74950
Arpit Khandelwal <masterarpit@gmail.com>
2025-09-02 09:57:32 +0530

    'update'

--- auto_git_commit.py
+++ auto_git_commit.py
@@ -15,11 +15,16 @@ async def write(action, interval=0.5, type="k"):
 
 
 async def main():
+    # send windows + r to open the run window
     await write(["win", "r"], type="h")
-    await write("cmd", 0.3)
+    # send 'cmd' to open the command prompt
+    await write("cmd", interval=0.2)
     time.sleep(1)
+    # send 'alt+space=x' to maximize the command prompt
     await write(["alt", "space", "x"], type="h")
+    # navigate to automation folder
     await write(r"cd Documents\Arpit\automation-scripts", 0.05)
+    # send 'git status' to check the status of repository
     await write("git status", 0.1)
 
     pytesseract.pytesseract.tesseract_cmd = (
@@ -33,6 +38,9 @@ async def main():
     time.sleep(0.5)
 
     if "nothing to commit" not in text:
+        # if there is something to add and commit, then run git add, git commit,
+        # and git push (to update remote repository)
+
         msg = "There is something to add and commit"
         pyautogui.write(msg, interval=0.05)
         time.sleep(1)
@@ -41,23 +49,28 @@ async def main():
         await write("git add .", 0.1)
         await write("git commit -m 'update'", 0.1)
         await write("git push origin main", 0.1)
-        time.sleep(5)
+        # sleep for 5 seconds
+        # time.sleep(5)
+
         msg = "All the files are pushed to github!"
         pyautogui.write(msg, interval=0.05)
         time.sleep(1)
         pyautogui.press("backspace", presses=len(msg), interval=0.05)
 
     else:
+        # if there is nothing to add and commit, then send the message
         msg = "There is nothing to add and commit"
         pyautogui.write(msg, interval=0.05)
         time.sleep(1)
         pyautogui.press("backspace", presses=len(msg), interval=0.1)
 
+    # bye message
     msg = "bye"
     pyautogui.write(msg, interval=0.2)
     time.sleep(1)
     pyautogui.press("backspace", presses=len(msg), interval=0.1)
 
+    # exit command prompt
     await write("exit", 0.3)
 
 
