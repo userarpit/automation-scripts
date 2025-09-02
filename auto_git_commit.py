fd77012d79e6deac51831fe593f8afbbdd9faf37
Arpit Khandelwal <masterarpit@gmail.com>
2025-09-02 09:22:36 +0530

    'update'

--- auto_git_commit.py
+++ auto_git_commit.py
@@ -41,10 +41,10 @@ async def main():
         await write("git add .", 0.1)
         await write("git commit -m 'update'", 0.1)
         await write("git push origin main", 0.1)
-
-        msg = "bye"
-        pyautogui.write(msg, interval=0.1)
         time.sleep(5)
+        msg = "Everything is pushed to github!"
+        pyautogui.write(msg, interval=0.1)
+        time.sleep(2)
         pyautogui.press("backspace", presses=len(msg), interval=0.1)
 
         # await write("exit", 0.5)
